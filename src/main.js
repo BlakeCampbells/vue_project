bc80a393e1c1574e01c67a07255b397a31eec67e
Blake Campbell <blakecampbell.dev@gmail.com>
2017-07-06 11:26:28 -0700

    Adding vue-router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
 import 'font-awesome/css/font-awesome.css'
 import App from './App'
-// import CustomFooter from './CustomFooter'
-Vue.use(VueResource)
+
+const router = new VueRouter({
+  routes: [
+    { path: '*', component: App }
+  ]
+})
+Vue.use(VueResource, router)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
